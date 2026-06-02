77f1172c3d780a302ad60c785d299a96e6cb9492
fox520-sketch <fox520486@gmail.com>
2026-06-02 20:47:46 +0800

    Add files via upload

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -7,12 +7,12 @@
 // 4. 不要放 serviceAccountKey.json 或任何私鑰到 GitHub。
 
 export const firebaseConfig = {
-  apiKey: 'PASTE_YOUR_API_KEY',
-  authDomain: 'PASTE_YOUR_PROJECT_ID.firebaseapp.com',
-  projectId: 'PASTE_YOUR_PROJECT_ID',
-  storageBucket: 'PASTE_YOUR_PROJECT_ID.appspot.com',
-  messagingSenderId: 'PASTE_YOUR_MESSAGING_SENDER_ID',
-  appId: 'PASTE_YOUR_APP_ID'
+  apiKey: 'AIzaSyDy8sWFzjOdAtYJUcRuzn3dvyJ0q2swESA',
+  authDomain: 'big2-tw.firebaseapp.com',
+  projectId: 'big2-tw',
+  storageBucket: 'big2-tw.firebasestorage.app',
+  messagingSenderId: '487558943667',
+  appId: '1:487558943667:web:2b916d4ec3561ad15031c6'
 };
 
 export function hasFirebaseConfig() {
